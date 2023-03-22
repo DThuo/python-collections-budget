17f675c33dcf2be05482b2f5ed8598c1dfd20e45
Thuo <89732702+DThuo@users.noreply.github.com>
2023-03-22 14:36:31 +1100

    Budgetlist

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 import Expense 
+from matplotlib.pyplot as plt
 
 
 #Create the BudgetList class and constructor
@@ -9,7 +10,6 @@ class BudgetList:
         self.expenses = []
         self.sum_overages = 0
         self.overages = []
-
 
 
 #define the append method
@@ -27,6 +27,24 @@ def __len__(self):
     return self.expenses.__len__ + self.overanges.__len__
 
 
+#Create and Finish the __iter__() method
+
+def __iter__(self):
+    self.iter_e = iter (self.expenses)
+    self.iter_o = iter(self.overages)
+    return self
+
+
+#Create next()
+
+def __next__(self):
+    try:
+        return self.iter_e.__next__()
+    except StopIteration as stop:
+        return self.iter_o.__next__()
+    
+
+
 #Define the main function
 
 def main():
@@ -36,8 +54,15 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
+
     print('The count of all expenses: ' + str(len(myBudgetList)))  #Print the Length of myBudgetList
+    for entry in myBudgetList:
+        print(entry)
+    fig, ax = plt.subplots()
+    labels = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
 
-
-if __name__ == "__maina__":
+if __name__ == "__main__":
     main()
