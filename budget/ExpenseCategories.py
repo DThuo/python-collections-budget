357502abdc576ddc141ec4d7f295e87c556c92f1
Thuo <89732702+DThuo@users.noreply.github.com>
2023-07-26 22:01:46 +1000

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,11 +1,13 @@
 from . import Expense
 import matplotlib.pyplot as plt
+import timeit
+
+
 
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
-
 
 if __name__ == "__main__":
     main()
